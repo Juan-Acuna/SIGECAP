c4cbe98b5a3d89466e158890b4cd9f563c8126a4
= <=>
2024-07-08 23:12:02 -0400

    commit inicial

---
+++ src/main/java/maryjaneslastdance/sigecap/SigecapApplication.java
@@ -0,0 +1,13 @@
+package maryjaneslastdance.sigecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigecapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maryjaneslastdance/sigecap/SigecapApplicationTests.java
@@ -0,0 +1,13 @@
+package maryjaneslastdance.sigecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
