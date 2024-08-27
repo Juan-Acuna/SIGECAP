b1a163c59acf2c5171a417ab1a04ae1a6bc97a9b
glopez-devops <gonzalo.lopez@itacademy.cl>
2024-08-26 23:38:05 -0400

    Se agregan nuevas pruebas búsqueda de título y resultado de busqueda

--- src/test/java/OpenBrowsersTest.java
+++ src/test/java/OpenBrowsersTest.java
@@ -16,9 +16,22 @@ public class OpenBrowsersTest {
 		System.setProperty("webdriver.gecko.driver","E:\\DEVOPS\\SIGECAP\\drivers\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.com");
+		if (driver.getTitle().contains("Google")) {
+            System.out.println("Título de la página es correcto.");
+        } else {
+            System.out.println("Título de la página es incorrecto.");
+        }
+		
 		WebElement e = driver.findElement(By.name("q"));
 		e.sendKeys("devops");
 		e.sendKeys(Keys.ENTER);
+		
+        if (driver.getPageSource().contains("devops")) {
+            System.out.println("Resultados de búsqueda están presentes.");
+        } else {
+            System.out.println("Resultados de búsqueda no están presentes.");
+        }		
+		
 		Thread.sleep(10000);
 		driver.quit();
 	}
