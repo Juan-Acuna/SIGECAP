07418da015333898d3991b0b86629bc29ee1af0b
glopez-devops <gonzalo.lopez@itacademy.cl>
2024-08-26 23:10:32 -0400

    Se agrega la clase para hacer pruebas funcionales con Selenium y navegadores Chrome y Firefox

--- src/test/java/OpenBrowsersTest.java
+++ src/test/java/OpenBrowsersTest.java
@@ -1,7 +1,12 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.mysql.cj.x.protobuf.MysqlxExpect.Open.Condition.Key;
 
 
 public class OpenBrowsersTest {
@@ -11,15 +16,18 @@ public class OpenBrowsersTest {
 		System.setProperty("webdriver.gecko.driver","E:\\DEVOPS\\SIGECAP\\drivers\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.com");
+		WebElement e = driver.findElement(By.name("q"));
+		e.sendKeys("devops");
+		e.sendKeys(Keys.ENTER);
 		Thread.sleep(10000);
 		driver.quit();
 	}
 	
-	/*
-	 * @Test public void openChrome() throws InterruptedException{
-	 * System.setProperty("webdriver.chrome.driver",
-	 * "E:\\DEVOPS\\SIGECAP\\drivers\\chromedriver.exe"); WebDriver driver = new
-	 * ChromeDriver(); driver.get("https://www.google.com"); Thread.sleep(10000);
-	 * driver.quit(); }
-	 */
+	
+	  @Test public void openChrome() throws InterruptedException{
+	  System.setProperty("webdriver.chrome.driver",
+	  "E:\\DEVOPS\\SIGECAP\\drivers\\chromedriver.exe"); WebDriver driver = new
+	  ChromeDriver(); driver.get("https://www.google.com"); Thread.sleep(10000);
+	  driver.quit(); }
+	 
 }
