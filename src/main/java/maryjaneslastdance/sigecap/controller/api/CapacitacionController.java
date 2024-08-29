e78bb142d824780765339900448de0bcfe146f28
= <=>
2024-08-28 23:14:06 -0400

    Preparacion para controlador de capacitacion

--- src/main/java/maryjaneslastdance/sigecap/controller/api/CapacitacionController.java
+++ src/main/java/maryjaneslastdance/sigecap/controller/api/CapacitacionController.java
@@ -1,6 +1,7 @@
 package maryjaneslastdance.sigecap.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import maryjaneslastdance.sigecap.service.CapacitacionService;
@@ -12,4 +13,10 @@ public class CapacitacionController{
 	@Autowired
 	CapacitacionService service;
 	
+	@GetMapping("/validar/")
+	public boolean validarDisponibilidad() {
+		
+		
+		return true;
+	}
 }
