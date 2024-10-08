f05e01dc1beb3787f95f0fc710209eea502a5a33
= <=>
2024-10-08 16:44:14 -0300

    Cambios en codigo para compatibilidad con loadBalancer

--- src/main/java/maryjaneslastdance/sigecap/SigecapApplication.java
+++ src/main/java/maryjaneslastdance/sigecap/SigecapApplication.java
@@ -3,11 +3,13 @@ package maryjaneslastdance.sigecap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.crypto.KeyGenerator;
+import java.security.NoSuchAlgorithmException;
+
 @SpringBootApplication
 public class SigecapApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException {
 		SpringApplication.run(SigecapApplication.class, args);
-		
 	}
 }
